d7cd219859d17e22ac9293597a7d1a51428a22c5
cavalcantgus <marcelo.cavalcanti.sousa@gmail.com>
2024-09-12 20:14:03 -0300

    Improving header and footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,9 @@
 <template>
-    <p>Footer</p>
+    <div>
+        <footer id="footer">
+            <p>Make your burguer &copy; 2024</p>
+        </footer>
+    </div>
 </template>
 
 <script>
@@ -7,3 +11,17 @@ export default {
     name: 'Footer'
 }
 </script>
+
+<style scoped>
+    #footer {
+        height: 200px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        color: rgb(255, 196, 0);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        bottom: 0;
+        width: 100%;
+    }
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <router-link>
-            <img src="" id="logo">
+    <div id="nav">
+        <router-link to="/" id="logo-url">
+            <img :src="logo" :alt="alt" id="logo">
         </router-link>
         <router-link to="/">Home</router-link> |
         <router-link to="/pedidos">Pedidos</router-link>
@@ -10,10 +10,42 @@
 
 <script>
 export default {
-    name: 'Navbar'
+    name: 'Navbar',
+    props: ['logo', 'alt']
 }
 </script>
 
 <style scoped>
+    #nav {
+        background-color: #222;
+        border-bottom: 4px solid #111;
+        padding: 15px 20px;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+    }
 
+    #nav #logo-url {
+        margin: auto;
+        margin-left: 0;
+    }
+
+    #nav #logo {
+        width: 40px;
+        height: 40px;
+    }
+
+    #nav a {
+        color: #2c2828;
+        text-decoration: none;
+        background-color: rgb(255, 196, 0);
+        border-radius: 10px;
+        padding: 10px;
+        margin: 12px;
+        transition: .5s;
+    }
+
+    #nav a:hover {
+        color: rgb(255, 255, 255);
+    }
 </style>
