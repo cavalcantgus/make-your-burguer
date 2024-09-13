3381babe66ee828b818cedf3bc1fd4551fff2de6
cavalcantgus <marcelo.cavalcanti.sousa@gmail.com>
2024-09-13 15:31:48 -0300

    Creating order form

---
+++ src/components/BurguerForm.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-dialog>
+        <v-card>
+            <v-card-title>
+                <span>Make your burguer</span>
+                <v-form ref="form">
+                    <v-text-field type="text"
+                    label="Nome do cliente"></v-text-field>
+                    <v-combobox label="Escolha o pão"></v-combobox>
+                    <v-combobox label="Escolha a carne do seu Burguer"></v-combobox>
+                </v-form>
+            </v-card-title>
+        </v-card>
+    </v-dialog>
+</template>
